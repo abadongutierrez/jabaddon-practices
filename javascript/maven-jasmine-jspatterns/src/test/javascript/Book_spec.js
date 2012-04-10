37fcb642b7186ba0757fd2a3ed8dbcde4f395b57
Rafael Gutierrez <abadon.gutierrez@gmail.com>
2012-04-10 01:58:20 -0500

    Se agregaron specs

--- javascript/maven-jasmine-jspatterns/src/test/javascript/Book_spec.js
+++ javascript/maven-jasmine-jspatterns/src/test/javascript/Book_spec.js
@@ -1,6 +1,16 @@
 describe("The Book", function() {
+    it("getAuthor is a function", function() {
+        var book = new Book();
+        expect(book.getAuthor instanceof Function).toBe(true);
+    });
+    
     it("author must be 'none' when new Book", function() {
         var book = new Book();
         expect(book.getAuthor()).toBe("none");
+    });
+    
+    it("author property is accesible", function() {
+        var book = new Book();
+        expect(book.author).toBe("none");
     });
 });
